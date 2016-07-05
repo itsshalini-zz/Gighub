c523ee9f97161a487e3061011f71b0925c51027e
its_shalini16 <its_shalini16@yahoo.com>
2016-07-05 19:32:21 +0000

    Add name to artist

--- Gighub/Gighub/ViewModels/FutureDate.cs
+++ Gighub/Gighub/ViewModels/FutureDate.cs
@@ -10,7 +10,7 @@ namespace Gighub.ViewModels
         {
             DateTime dateTime;
             var isValid = DateTime.TryParseExact(Convert.ToString(value),
-                "dd mmm yyyy",
+                "mm dd yyyy",
                 CultureInfo.CurrentCulture, 
                 DateTimeStyles.None,
                 out dateTime);
