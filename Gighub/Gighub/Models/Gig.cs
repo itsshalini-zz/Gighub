df44a9cbb095aea5bb26231be5326ca6b99757c8
its_shalini16 <its_shalini16@yahoo.com>
2016-07-11 11:11:15 +0000

    Refractor Gigs controller

--- Gighub/Gighub/Models/Gig.cs
+++ Gighub/Gighub/Models/Gig.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Gighub.Models
 {
     public class Gig
     {
-        public int Id { get; set; }
+        public int Id { get;private set; }
 
-        public bool IsCanceled { get; set; }
+        public bool IsCanceled { get;private set; }
 
         public ApplicationUser Artist { get; set; }
 
@@ -24,5 +27,38 @@ namespace Gighub.Models
 
         [Required]
         public byte GenreId { get; set; }
+
+        public ICollection<Attendance> Attendances { get;private set; }
+
+        public Gig()
+        {
+            Attendances = new Collection<Attendance>();
+        }
+
+        public void Cancel()
+        {
+            IsCanceled = true;
+
+            var notification = Notification.GigCanceled(this);
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+        }
+
+        public void Update(DateTime dateTime, string venue, byte genre)
+        {
+            var notification = Notification.GigUpdated(this, DateTime, Venue);
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+
+            this.DateTime = dateTime;
+            Venue = venue;
+            this.GenreId = genre;
+        }
     }
 }
