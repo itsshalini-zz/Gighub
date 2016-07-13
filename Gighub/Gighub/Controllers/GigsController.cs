c7d9fdb5e51d4cfe4b17d41f6984d0e2b6f7e9b9
its_shalini16 <its_shalini16@yahoo.com>
2016-07-13 14:52:24 +0000

    Improved look and feel

--- Gighub/Gighub/Controllers/GigsController.cs
+++ Gighub/Gighub/Controllers/GigsController.cs
@@ -17,6 +17,12 @@ namespace Gighub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [HttpPost]
+        public ActionResult Search(GigsViewModel viewModel)
+        {
+            return RedirectToAction("Index", "Home", new { query = viewModel.SearchTerm });
+        }
+
         [Authorize]
         public ActionResult Mine()
         {
--- Gighub/Gighub/Controllers/HomeController.cs
+++ Gighub/Gighub/Controllers/HomeController.cs
@@ -15,22 +15,32 @@ namespace Gighub.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        public ActionResult Index()
+        public ActionResult Index(string query = null)
         {
+
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
-                .Include(g=>g.Genre)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now
                     && !g.IsCanceled);
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                upcomingGigs = upcomingGigs
+                    .Where(g => g.Artist.Name.Contains(query) ||
+                    g.Genre.Name.Contains(query) ||
+                    g.Venue.Contains(query));
+            }
 
             var viewModel = new GigsViewModel
             {
                 UpcomingGigs = upcomingGigs,
                 ShowActions = User.Identity.IsAuthenticated,
-                Heading= "Upcoming Gigs"
+                Heading = "Upcoming Gigs",
+                SearchTerm = query
             };
 
-            return View("Gigs",viewModel);
+            return View("Gigs", viewModel);
         }
 
         public ActionResult About()
